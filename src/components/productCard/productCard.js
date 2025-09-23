c8ed13ffaaa3ce275c6c22bd293a49ba3d0b1451
STEVEN_HOSKINS <steve.hoskins@theentertainer.com>
2025-09-23 08:55:49 +0100

    ios shadow

--- src/components/productCard/productCard.js
+++ src/components/productCard/productCard.js
@@ -110,9 +110,9 @@ const ProductCard = ({ product }) => {
       {/* Quick View Modal */}
       {quickViewProduct && (
         <div className="fixed inset-0 z-[700] bg-black/60">
-          <div ref={wrapperRef} className="fixed left-[50%] top-[50%] z-50 w-full translate-x-[-50%] translate-y-[-50%] sm:rounded-lg max-w-[90vw] max-h-[90vh]">
-            <div className="overflow-y-auto shadow-text-blue bg-gradient-to-br from-black via-gray-900 to-black h-full w-full gap-4 p-4 grid max-h-[90vh]">
-            <div className="relative w-full flex flex-col gap-4 drop-shadow-none">
+          <div ref={wrapperRef} className="fixed left-[50%] top-[50%] z-50 w-full translate-x-[-50%] translate-y-[-50%] p-2 sm:rounded-lg max-w-[90vw] max-h-[90vh]">
+            <div className="overflow-y-auto shadow-text-blue bg-gradient-to-br from-black via-gray-900 to-black h-full gap-4 p-4 grid max-h-[calc(90vh-20px)]">
+            <div className="relative flex flex-col gap-4 drop-shadow-none">
               {quickViewProduct && (
                 <>
                   <button
@@ -140,7 +140,7 @@ const ProductCard = ({ product }) => {
 
                   <div className="grid md:grid-cols-2 gap-6">
                     {/* Product Images */}
-                    <div className="max-w-[calc(90vw-37px)]">
+                    <div className="max-w-[calc(90vw-48px)]">
                       <div className="relative">
                         <Swiper
                           modules={[Autoplay, Pagination]}
